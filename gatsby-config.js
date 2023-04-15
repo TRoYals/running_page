79c736b371565fa388f087a493ac713e74cf6321
FU QIXUAN <59471250+TRoYals@users.noreply.github.com>
2023-04-15 10:42:54 +0800

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,17 +2,17 @@ module.exports = {
   pathPrefix: process.env.PATH_PREFIX || '/',
   siteMetadata: {
     siteTitle: 'Running Page',
-    siteUrl: 'https://yihong.run',
+    siteUrl: 'https://run.naglfar28.com,
     logo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQTtc69JxHNcmN1ETpMUX4dozAgAN6iPjWalQ&usqp=CAU',
-    description: 'Personal site and blog',
+    description: 'Just Run',
     navLinks: [
       {
         name: 'Blog',
-        url: 'https://yihong.run/running',
+        url: 'https://run.naglfar28.com',
       },
       {
         name: 'About',
-        url: 'https://github.com/yihong0618/running_page/blob/master/README-CN.md',
+        url: 'https://run.naglfar28.com',
       },
     ],
   },
